6c2a7784acad95b453ea6d9116e15684cb6d8e76
MusiKernel Team <musikernel@nospam.org>
2015-06-24 08:09:26 -0400

    Add copyright notices to the source files

--- treesnake/__init__.py
+++ treesnake/__init__.py
@@ -1,3 +1,16 @@
+"""
+This file is part of the treesnake project, Copyright Jeff Hubbard
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+"""
+
 
 class BinaryTreeNode:
     def __init__(self, value, left=None, right=None, parent=None):
