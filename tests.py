1fb09b56d3e1627b7891985598a450c8165723a0
MusiKernel Team <musikernel@nospam.org>
2015-06-23 20:56:22 -0400

    Convert the recursive is_bst method to iterative to avoid the recursion depth limit.
    (and add some more unit tests)

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import unittest
 from treesnake import *
 
 class BstTests(unittest.TestCase):
-    def  test_is_bst(self):
+    def test_random_is_bst(self):
         import random
         tree = BinarySearchTree()
         values = []
@@ -20,6 +20,18 @@ class BstTests(unittest.TestCase):
 #            tree.delete(value)
 #        self.assertTrue(tree.is_bst())
 
+    def test_empty_is_bst(self):
+        tree = BinarySearchTree()
+        self.assertTrue(tree.is_bst())
+
+    def test_known_bad_is_not_bst(self):
+        tree = BinarySearchTree()
+        tree.first_node = node = BinaryTreeNode(10)
+        node.left = BinaryTreeNode(5)
+        node.right = node = BinaryTreeNode(15)
+        node.left = BinaryTreeNode(6)  # This one is not valid
+        node.right = BinaryTreeNode(20)
+        self.assertFalse(tree.is_bst())
 
 if __name__ == "__main__":
     unittest.main()
--- treesnake/__init__.py
+++ treesnake/__init__.py
@@ -59,24 +59,22 @@ class BinarySearchTree(BinaryTree):
         pass
 
     def is_bst(self):
-        if not self.first_node:
-            return True
-        return self._is_bst(self.first_node, None, None)
+        stack = []
+        node = self.first_node
+        last_node = None
 
-    def _is_bst(self, node, min_val, max_val):
-        if node is None:
-            return True
-        min_arg = None if min_val is None else min([node.value, max_val])
-        max_arg = None if max_val is None else max([node.value, min_val])
-        if((min_val is None or node.value > min_val) and
-        (max_val is None or node.value < max_val) and
-        self._is_bst(node.left, min_val, min_arg) and
-        self._is_bst(node.right, max_arg, max_val)):
-            return True
-        else:
-            return False
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                if last_node and node < last_node:
+                    return False
+                last_node = node
+                node = node.right
 
-
+        return True
 
 class RedBlackNode(BinaryTreeNode):
     Red = 0
