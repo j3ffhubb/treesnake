9f132f1d6f3be3fab097fcbba12248ae8318bbb6
MusiKernel Team <musikernel@nospam.org>
2015-06-24 08:15:37 -0400

    Add a setup.py script

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+This file is part of the treesnake project, Copyright Jeff Hubbard
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+"""
+
+import setuptools
+import os
+import sys
+
+setuptools.setup(
+    name="treesnake",
+    version="1.0",
+    author="Jeff Hubbard",
+    author_email='musikernel@nospam.org',
+    license='GPL 3.0',
+    description='Tree-family data structure',
+    url='https://github.com/j3ffhubb/treesnake',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
+
